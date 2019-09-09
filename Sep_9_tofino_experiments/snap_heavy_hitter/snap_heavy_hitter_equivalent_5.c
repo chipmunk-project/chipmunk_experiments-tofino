51a3ecb27c5da52d43a5d346e57c10f7dcaaf651
Ubuntu <ubuntu@ip-172-31-44-213.us-east-2.compute.internal>
2019-09-09 04:12:08 +0000

    Get the compilation result for tofino

---
+++ Sep_9_tofino_experiments/snap_heavy_hitter/snap_heavy_hitter_equivalent_5.c
@@ -0,0 +1,12 @@
+struct Packet{
+int srcip;
+};
+int heavy_hitter[1000000] = {0};
+int hh_counter[1000000] = {0};
+void func(struct Packet p){
+p.srcip=p.srcip;
+if (1==1&&heavy_hitter[p.srcip]==0&&1==1&&1==1) {hh_counter[p.srcip]=hh_counter[p.srcip]+1;
+if (1==1&&hh_counter[p.srcip]==1000&&1==1&&1==1) {heavy_hitter[p.srcip]=1;
+;}
+;}
+}
