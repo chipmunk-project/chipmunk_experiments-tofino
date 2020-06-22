2d25b2acf42035f06e53ccb660030c5f76ebe24d
Xiangyu Gao <xg673@nyu.edu>
2020-06-22 20:26:59 +0800

    Run slicing from python script

---
+++ build_map.py
@@ -0,0 +1,191 @@
+import re
+import subprocess
+
+def print_dic(given_dic):
+    for x, y in given_dic.items():
+        print(x, "<-----------", y)
+
+# if list_x is a subset of list_y return true
+def contain(list_x, list_y):
+    if len(list_x) > len(list_y):
+        return False
+    for i in range(len(list_x)):
+        if list_x[i] not in list_y:
+            return False
+    return True
+
+def generate_cmd_line(program_file, group_size, bit_size_for_constant_set):
+    # Run domino
+    (ret_code, output) = subprocess.getstatusoutput("domino " + program_file + " hh.sk 10 10")
+    
+    # Run canonicalizer
+    (ret_code, output) = subprocess.getstatusoutput("canonicalizer " + program_file)
+    assert ret_code == 0, "canonicalizer failed"
+    
+    canonicalizer_file ="/tmp/" + program_file[program_file.rfind('/') + 1 : program_file.rfind('.')] + "_canonicalizer.c"
+    # grouper
+    (ret_code, total_num_of_grouped_files) = subprocess.getstatusoutput("grouper " + canonicalizer_file + " " + group_size)
+    assert ret_code == 0, "grouper failed"
+    
+    group_file = "/tmp/" + canonicalizer_file[canonicalizer_file.rfind('/') + 1:canonicalizer_file.rfind('.')] + "_equivalent_" + str(int(total_num_of_grouped_files) - 1) + ".c"
+
+    (ret_code, constant_set) = subprocess.getstatusoutput(("constant_set " + group_file + " " + bit_size_for_constant_set))
+    assert ret_code == 0, "constant_set failed"
+    # domino_to_chipmunk
+    (ret_code, output) = subprocess.getstatusoutput("domino_to_chipmunk " + group_file)
+    assert ret_code == 0, "domino_to_chipmunk failed"
+    
+    # build dic for each file
+    file_name_list = ["/tmp/canonicalizer_map.txt", "/tmp/grouper_map.txt", "/tmp/domino_to_chipmunk_map.txt"]
+    dic_list = [{}, {}, {}]
+    
+    # read from each file from file_name_list and put them into the dict
+    for i in range(len(file_name_list)):
+        filename = file_name_list[i]
+        f = open(filename, "r")
+        for x in f:
+            key = x.split(':')[0]
+            val = x.split(':')[1].split(',')
+            assert len(val) == 1, "wrong length of val"
+            if val[-1][-1] == '\n':
+                val[-1] = val[-1][:-1]
+            dic_list[i][key] = val[0]
+    
+    # propagate dic_list
+    for i in range(1, len(file_name_list)):
+        for x in dic_list[0]:
+            if dic_list[0][x] in dic_list[i]:
+                 dic_list[0][x] = dic_list[i][dic_list[0][x]]
+    
+    # Build the influ_dic
+    # i.e. last_update:last_update,p.now --> key: last_update; val: [p.now, last_update]
+    influ_dic = {}
+    influence_map_file = "/tmp/influence_map.txt"
+    f = open(influence_map_file, "r")
+    for x in f:
+        key = x.split(':')[0]
+        val = x.split(':')[1].split(',')
+        # remove the '\n' char
+        if val[-1][-1] == '\n':
+            val[-1] = val[-1][:-1]
+        for i in range(len(val)):
+            val[i] = dic_list[0][val[i]]
+        influ_dic[dic_list[0][key]] = val
+    
+    # record how many stateful groups and how many packet fields
+    total_state_groups = []
+    total_packet_fields = []
+    for x in influ_dic:
+        if x.find("state_group_") != -1:
+            group_num = re.findall("state_and_packet.state_group_(\d+)", x)
+            if group_num[0] not in total_state_groups:
+                total_state_groups.append(group_num[0])
+        else:
+            assert x.find("pkt_") != -1
+            pkt_field = re.findall("state_and_packet.pkt_(\d+)", x)
+            total_packet_fields.append(pkt_field[0])
+            
+        
+    # Keep ascending order
+    total_state_groups.sort()
+    total_packet_fields.sort()
+    
+    # build the cmd line str
+    cmd_line_list = []
+    # build cmd line str for packet fields
+    for i in range(len(total_packet_fields)):
+        state_groups = []
+        input_packet = []
+        key = "state_and_packet.pkt_" + str(total_packet_fields[i])
+        for x in influ_dic[key]:
+            if x.find("state_group_") != -1:
+                group_num = re.findall("state_and_packet.state_group_(\d+)", x)[0]
+                if group_num not in state_groups:
+                    state_groups.append(group_num)
+                if group_num in total_state_groups:
+                    total_state_groups.remove(group_num)
+            else:
+                assert x.find("pkt_") != -1
+                pkt_num = re.findall("state_and_packet.pkt_(\d+)", x)[0]
+                if pkt_num not in input_packet:
+                    input_packet.append(pkt_num)
+        state_groups.sort()
+        input_packet.sort()
+        # build the cmd line
+        cmd_str = " --pkt-fields " + str(total_packet_fields[i])
+        if len(state_groups) != 0:
+            cmd_str += " --state-groups "
+            for index in state_groups:
+                 cmd_str += str(index) + " "
+        if len(input_packet) == 0:
+            # only set the input packet to be 0
+            cmd_str += " --input-packet 0"
+        else:
+            cmd_str += " --input-packet "
+            for index in input_packet:
+                cmd_str += str(index) + " "
+        cmd_line_list.append(cmd_str)
+
+    state_dic = {}
+    # Find influencer
+    for i in range(len(total_state_groups)):
+        state_groups = []
+        # only need to take care of the state_0 in each state group
+        key = "state_and_packet.state_group_" + str(total_state_groups[i]) + "_state_0"
+        for x in influ_dic[key]:
+            if x.find("state_group_") != -1:
+                group_num = re.findall("state_and_packet.state_group_(\d+)", x)[0]
+                if group_num not in state_groups:
+                    state_groups.append(group_num)
+        state_dic[key] = state_groups
+    remove_list_state = []
+    for x in state_dic:
+        for y in state_dic:
+            if x != y:
+                if (x not in remove_list_state) and contain(state_dic[x], state_dic[y]):
+                    remove_list_state.append(x)
+                elif (y not in remove_list_state) and contain(state_dic[y], state_dic[x]):
+                    remove_list_state.append(y)
+    
+    # build cmd line str for stateful vars
+    for i in range(len(total_state_groups)):
+        state_groups = []
+        input_packet = []
+        key = "state_and_packet.state_group_" + str(total_state_groups[i]) + "_state_0"
+        if key in remove_list_state:
+            continue
+        for x in influ_dic[key]:
+            if x.find("state_group_") != -1:
+                group_num = re.findall("state_and_packet.state_group_(\d+)", x)[0]
+                if group_num not in state_groups:
+                    state_groups.append(group_num)
+            else:
+                assert x.find("pkt_") != -1
+                pkt_num = re.findall("state_and_packet.pkt_(\d+)", x)[0]
+                if pkt_num not in input_packet:
+                    input_packet.append(pkt_num) 
+        state_groups.sort()
+        input_packet.sort()
+        # build the cmd line
+        cmd_str = " --state-groups "
+        if len(state_groups) != 0:
+            for index in state_groups:
+                 cmd_str += str(index) + " "
+        if len(input_packet) == 0:
+            # only set the input packet to be 0
+            cmd_str += " --input-packet 0"
+        else:
+            cmd_str += " --input-packet "
+            for index in input_packet:
+                cmd_str += str(index) + " "
+        cmd_line_list.append(cmd_str)
+    return cmd_line_list, constant_set, total_num_of_grouped_files
+
+program_file_list = ['learn_filter.c', 'blue_increase.c', 'blue_decrease.c', 'stateful_fw.c', 'dns_ttl_change.c', 'flowlets.c', 'rcp.c',
+                     'marple_new_flow.c', 'marple_tcp_nmo.c', 'sampling.c', 'stfq.c', 'conga.c', 'snap_heavy_hitter.c', 'spam_detection.c']
+group_size_list = ['1','1','1','1','1','1','1','1','1','1','1','2','2','2']
+
+for i in range(len(program_file_list)):
+    program_file = "/Users/Xiangyu/domino_example_test/domino-examples/domino_programs/" + program_file_list[i]
+    group_size = group_size_list[i] 
+    generate_cmd_line(program_file, group_size, "2")
--- compile_with_chipmunk.py
+++ compile_with_chipmunk.py
@@ -2,10 +2,11 @@
 #  ---> a_group_of_domino_files.cc --- (domino_to_chipmunk) ---> .sk files
 #  ---> run iterative_solver on each .sk file
 
+from build_map import generate_cmd_line
 import sys
 import re
 import subprocess
-
+import time
 
 def main(argv):
     """Main program."""
@@ -21,66 +22,58 @@ def main(argv):
     group_size = str(argv[2])
     stateful_alu_file = str(argv[3])
     stateless_alu_file = str(argv[4])
-    num_pipeline_stages = str(argv[5])
-    num_alus_per_stage = str(argv[6])
+    num_pipeline_stages = int(argv[5])
+    num_alus_per_stage = int(argv[6])
     input_bits = str(argv[7])
     bit_size_for_constant_set = str(argv[8])
 
-    # Run canonicalizer
-    (ret_code,
-     output) = subprocess.getstatusoutput("canonicalizer " + program_file)
+    cmd_line_list, constant_set, total_num_of_grouped_files = generate_cmd_line(program_file, group_size, bit_size_for_constant_set)
+    print(cmd_line_list)
+    print(constant_set)
+
     # Output canonicalizer file to /tmp/file_name_canonicalizer.c
     canonicalizer_file ="/tmp/" + program_file[program_file.rfind('/') + 1 : program_file.rfind('.')] + "_canonicalizer.c"
-    with open(canonicalizer_file, 'w') as file:
-        file.write(output)
-    # Run grouper to generate files with different group strategies output to /tmp folder
-    # the name of group_files is /tmp/<file_name>_equivalent_(/d).c
-    (ret_code,
-     output) = subprocess.getstatusoutput("grouper " + canonicalizer_file +
-                                          " " + group_size)
-    print(output)
+    
     # Output restore the total number of group files
     # the name of group_files is /tmp/<file_name>_canonicalizer_equivalent_(/d).sk
-    total_num_of_grouped_files = int(output)
-    for i in range(total_num_of_grouped_files):
-        group_file = "/tmp/" + canonicalizer_file[canonicalizer_file.rfind(
-            '/') + 1:canonicalizer_file.rfind('.')] + "_equivalent_" + str(i) + ".c"
-        (ret_code, output) = subprocess.getstatusoutput("domino_to_chipmunk " +
-                                                        group_file)
-        (ret_code, constant_set) = subprocess.getstatusoutput("constant_set " + \
-                                                             group_file + " " +\
-                                                             bit_size_for_constant_set)
-        constant_set = "'" + constant_set + "'"
-        chipmunk_file = group_file[:group_file.rfind('.')] + ".sk"
-        with open(chipmunk_file, 'w') as file:
-            file.write(output)
+    group_file = "/tmp/" + canonicalizer_file[canonicalizer_file.rfind('/') + 1:canonicalizer_file.rfind('.')] + "_equivalent_" + str(int(total_num_of_grouped_files) - 1) + ".c"
+
+
     # Run iterative solver for grouped files
-    for i in range(total_num_of_grouped_files):
-        sketch_file_name = "/tmp/" + canonicalizer_file[canonicalizer_file.rfind(
-            '/') + 1:canonicalizer_file.rfind('.')] + "_equivalent_" + str(i) +".sk"
-        # Print the current situation
-        print("Current compilation file is " + sketch_file_name)
-        # Get the string to run in terminal
-        str_to_run_in_terminal = "iterative_solver " + sketch_file_name + " " + stateful_alu_file + " " + stateless_alu_file + " " + \
-                                 num_pipeline_stages + " " + num_alus_per_stage + " " + \
-                                 constant_set + " " + input_bits + " --parallel-sketch"
-        (ret_code, output) = subprocess.getstatusoutput(str_to_run_in_terminal)
-        iterative_solver_output_file_name = '/tmp/' + \
-					    sketch_file_name[sketch_file_name.rfind('/') + 1:sketch_file_name.rfind('.')] + '_' + \
+    sketch_file_name = "/tmp/" + canonicalizer_file[canonicalizer_file.rfind('/') + 1:canonicalizer_file.rfind('.')] + "_equivalent_" + str(int(total_num_of_grouped_files) - 1) +".sk"
+    constant_set = "'" + constant_set + "'"
+    for i in range(len(cmd_line_list)):
+        flag = 0
+        for depth in range(1, num_pipeline_stages + 1):
+            if flag == 1:
+                break
+            for width in range(1, num_alus_per_stage + 1):
+                print("depth = ", depth, " width = ", width, " flag = ", flag)
+                str_to_run_in_terminal = "iterative_solver " + sketch_file_name + " " + stateful_alu_file + " " + stateless_alu_file + " " + \
+                                 str(depth) + " " + str(width) + " " + \
+                                 constant_set + " " + input_bits + " --parallel-sketch " + cmd_line_list[i]
+                time_start=time.time()
+                print(str_to_run_in_terminal)
+                (ret_code, output) = subprocess.getstatusoutput(str_to_run_in_terminal)
+                iterative_solver_output_file_name = '/tmp/' + \
+                                            sketch_file_name[sketch_file_name.rfind('/') + 1:sketch_file_name.rfind('.')] + '_' + \
                                             'with_stateful_alu' + '_' + \
                                             stateful_alu_file[stateful_alu_file.rfind('/') + 1:stateful_alu_file.rfind('.')] + '_' + \
                                             'with_stateless_alu' + '_' + \
                                             stateless_alu_file[stateless_alu_file.rfind('/') + 1:stateless_alu_file.rfind('.')] + \
-                                            '_' + num_pipeline_stages + '_' + num_alus_per_stage  + '.output'
-        with open(iterative_solver_output_file_name, 'w') as file:
-            file.write(output)
-        # It will return 0 if one of the grouped files get successful compilation
-        if (ret_code == 0):
-          print("Compilation succeeds for Program: " + program_file[program_file.rfind('/') + 1:] + ", with stateful alu: " + stateful_alu_file + "and stateless alu: " + stateless_alu_file + ", with grid size: " + num_pipeline_stages + " * " + num_alus_per_stage)
-          sys.exit(0)
-
-    print("Compilation fails for Program: " + program_file[program_file.rfind('/') + 1:] + ", with alu: " + stateful_alu_file + "and stateless alu: " + stateless_alu_file + ", with grid size: " + num_pipeline_stages + " * " + num_alus_per_stage)
-    sys.exit(1)
+                                            '_' + str(depth) + "_" + str(width) + '_slice_' + str(i + 1) + '.output'
+                with open(iterative_solver_output_file_name, 'w') as file:
+                    file.write(output)
+                # It will return 0 if one of the grouped files get successful compilation
+                if (ret_code == 0):
+                    time_end=time.time()
+                    print("Compilation succeeds for Program: " + program_file[program_file.rfind('/') + 1:] + ", with stateful alu: " + stateful_alu_file + " and stateless alu: " + stateless_alu_file + ", with grid size: " + str(depth) + " * " + str(width) + " in slice " + str(i + 1))
+                    print('time cost', round(time_end-time_start, 2),'s')
+                    flag = 1
+                    break
+        if flag == 0:
+            print("Compilation fails for Program: " + program_file[program_file.rfind('/') + 1:] + ", with alu: " + stateful_alu_file + " and stateless alu: " + stateless_alu_file + ", with grid size: " + str(num_pipeline_stages) + " * " + str(num_alus_per_stage) + " in slice No." + str(i + 1))
+            sys.exit(1)
 
 if __name__ == "__main__":
     main(sys.argv)
