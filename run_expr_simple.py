fa6d156d9112e86b10f63bcf0573ae6dcf68f68e
XiangyuG <xg673@nyu.edu>
2020-06-23 19:54:40 -0400

    Reverse the compilation order

--- run_expr_simple.py
+++ run_expr_simple.py
@@ -6,7 +6,7 @@ grid_size_list = [' 2 2 ', ' 2 2 ', ' 3 2 ', ' 2 1 ', ' 3 3 ', ' 1 2 ', ' 1 1 ',
 group_size_list = [' 1 ', ' 1 ', ' 1 ', ' 1 ', ' 1 ', ' 2 ', ' 2 ', ' 2 ']
 alu_list = ['pred_raw.alu', 'pred_raw.alu', 'pred_raw.alu', 'if_else_raw.alu', 'nested_ifs.alu', 'pair.alu', 'pair.alu', 'pair.alu']
 stateless_alu = 'example_alus/stateless_alus/stateless_alu.alu'
-for i in range(len(program_list)):
+for i in range(len(program_list) -1, -1, -1):
     domino_file_name = '../domino-examples/domino_programs/' + program_list[i]
     stateful_alu = 'example_alus/stateful_alus/' + alu_list[i]
     grid_size = grid_size_list[i]
